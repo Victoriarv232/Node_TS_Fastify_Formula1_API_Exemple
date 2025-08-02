c906e52ebb14f06d35bbb9eecfe87267b7247bd7
Victoriarv232 <victoriaresesendv@gmail.com>
2025-08-02 15:04:50 -0300

    Creating Project

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import fastify from "fastify";
+import cors from "@fastify/cors"
+
+const server = fastify({logger:true});
+
+server.register(cors, {
+    origin: "*",
+});
+
+const teams = [
+    {id: 1, name: "McLaren", base: "Woking, UK"},
+    {id: 2, name: "Mercedes", base: "Brackley, UK"},
+    {id: 3, name: "Red Bull Racing", base: "Milton Keynes, UK"},
+]
+
+server.get("/teams", async(request, response) => {
+    response.type("application/json").code(200);
+    return { teams };
+});
+
+const drivers = [
+    {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
+    {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
+    {id: 3, name: "Lando Norris", team: "McLaren"},
+]
+
+server.get("/drivers", async(request, response)=>{
+    response.type("application/json").code(200);
+    return { drivers };
+});
+
+
+interface DriverParams{
+    id: string
+}
+server.get<{Params: DriverParams}>("/drivers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find(d => d.id === id);
+
+    if (!driver){
+        response.type("application/json").code(404);
+        return {message: "Driver Not Found"};
+    }
+    else{
+        response.type("application/json").code(200);
+        return { driver }
+    }
+});
+
+server.listen({port: 3333}, () => {
+    console.log("Server init");
+});
